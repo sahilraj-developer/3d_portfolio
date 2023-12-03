d9d12332dad3b1d75e4ba8296dbb99d824823f92
sahil <sahil27raj@gmail.com>
2023-12-03 15:20:14 +0530

    add the navbar file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import About from './components/About';
+import Contact from './components/Contact';
+import Experience from './components/Experience';
+import Feedbacks from './components/Feedbacks';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Tech from './components/Tech';
+import Works from './components/Works';
+import StarsCanvas from './components/StarsCanvas';
+
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <div className='relative z-0 bg-promary'>
+          <div className='bg-her-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className='relative z-0'>
+            <Contact/>
+            <StarsCanvas/>
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
